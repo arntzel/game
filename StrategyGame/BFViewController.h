f7bb3b2df44333ca5bd705c526ffe13bba8d5fb3
Eliot <arntzel@gmail.com>
2012-10-22 13:33:27 -0400

    Initial Commit

---
+++ StrategyGame/BFViewController.h
@@ -0,0 +1,13 @@
+//
+//  BFViewController.h
+//  StrategyGame
+//
+//  Created by Eliot Arntz on 10/22/12.
+//  Copyright (c) 2012 Eliot Arntz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BFViewController : UIViewController
+
+@end
